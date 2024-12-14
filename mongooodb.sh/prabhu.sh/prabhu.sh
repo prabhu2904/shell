6e45c820148dc90f79068a4c5704f03b41059623
prabhu2904 <uyyalaprabhukumar77@gmail.com>
2024-12-13 21:57:54 -0800

    practice

--- mongooodb.sh/prabhu.sh/prabhu.sh
+++ mongooodb.sh/prabhu.sh/prabhu.sh
@@ -10,6 +10,10 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+
+}
+
 if [ $ID -ne 0 ]
 then
   echo "error :$R please run script with root access $N"
@@ -36,3 +40,26 @@ then
 else
   echo "redis installed sucess"
 fi
+p mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restarting MongoDB"
